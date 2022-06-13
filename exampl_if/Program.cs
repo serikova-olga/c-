ba2e41e5d5f508855b826cd282ad2968682cc195
OLGA <serikova-olga@mail.ru>
2022-06-13 17:39:11 +0300

    if

---
+++ exampl_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Input your name: ");
+string username = Console.ReadLine ();
+
+if(username.ToLower() == "alice")
+{
+    Console.WriteLine("Wow it is Alice!");
+}
+else
+{
+    Console.Write("Hello!");
+    Console.WriteLine(username);
+}
